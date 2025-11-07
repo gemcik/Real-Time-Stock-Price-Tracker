7b09c42e10f01a9df41825f47890cea7c9280989
megzz24 <meghnavs24@gmail.com>
2025-11-07 14:46:06 +0530

    feat: add WebSocket backend and frontend UI

---
+++ websocket-broadcast-service/src/test/java/com/gemcik/websocket_broadcast_service/WebsocketBroadcastServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemcik.websocket_broadcast_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketBroadcastServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
