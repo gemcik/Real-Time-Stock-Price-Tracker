a53c41b9aa2519d35740b69466fddf2cbc5aa426
Aadith N <135203735+aadithkichu@users.noreply.github.com>
2025-11-07 15:18:50 +0530

    feat: implement producer (#1)

---
+++ price-producer-service/src/test/java/com/gemcik/price_producer_service/PriceProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemcik.price_producer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
